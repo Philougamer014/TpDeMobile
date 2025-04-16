20255e91f6d3be42c186859f41353144f61df2c5
Philippee <philippe.leonard2006@gmail.com>
2025-04-16 14:11:49 -0400

    Initial Commit

---
+++ Tp3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tp3
+//
+//  Created by Philippe Léonard on 2025-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tp3/Tp3App.swift
@@ -0,0 +1,17 @@
+//
+//  Tp3App.swift
+//  Tp3
+//
+//  Created by Philippe Léonard on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct Tp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
